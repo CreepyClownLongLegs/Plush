20550e2e599051f0813b8a738d3f391e7d753635
Caroline Gritsch <e11831765@student.tuwien.ac.at>
2024-05-17 14:26:52 +0000

    Remove message code

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
-import { mapToCanActivate, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './guards/auth.guard';
-import { MessageComponent } from './components/message/message.component';
 import { AdminPlushtoyOverviewComponent } from './components/admin/plushtoy/overview/overview.component';
 import { AdminPlushtoyCreateComponent as AdminPlushtoyCreateComponent } from './components/admin/plushtoy/create/create.component';
 import { AdminCategoryCreateComponent } from './components/admin/categories/create/create.component';
 import { AdminCategoryOverviewComponent } from './components/admin/categories/overview/overview.component';
 
+
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'message', canActivate: mapToCanActivate([AuthGuard]), component: MessageComponent, data: { role: 'ADMIN' } },
   {
     path: 'admin', canActivate: [AuthGuard], data: { role: 'ADMIN' }, children: [
       { path: '', component: AdminPlushtoyOverviewComponent, data: { role: 'ADMIN' } },
