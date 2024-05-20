6bd04b2ee8ddd7e16331e090b02461f09f2d3029
Tobias Haas <e12220872@student.tuwien.ac.at>
2024-05-20 10:18:35 +0000

    #63: Fix authentication AdminEndpoint only returning Invalid authorization header or token

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/AuthService.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/AuthService.java
@@ -1,22 +1,25 @@
 package at.ac.tuwien.sepr.groupphase.backend.service;
 
-import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.JwtDto;
-import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.NonceDto;
 import org.springframework.security.authentication.BadCredentialsException;
+
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.AuthRequestDto;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.NonceDto;
 
 public interface AuthService {
     /**
-     * Authenticates a user using the provided credentials and generates a JWT token.
+     * Authenticates a user using the provided credentials and generates a JWT
+     * token.
      *
-     * @param userLoginDto The data transfer object containing the user's login credentials.
-     * @return JwtDto The data transfer object containing the generated JWT token.
+     * @param userLoginDto The data transfer object containing the user's login
+     *                     credentials.
+     * @return the authentication Jwt as String.
      * @throws BadCredentialsException If the provided credentials are invalid.
      */
-    JwtDto login(AuthRequestDto userLoginDto) throws BadCredentialsException;
+    String login(AuthRequestDto userLoginDto) throws BadCredentialsException;
 
     /**
-     * Generates a nonce for a given public key. The nonce is a random number used once in a cryptographic communication.
+     * Generates a nonce for a given public key. The nonce is a random number used
+     * once in a cryptographic communication.
      *
      * @param publicKey The public key for which to generate a nonce.
      * @return NonceDto The data transfer object containing the generated nonce.
