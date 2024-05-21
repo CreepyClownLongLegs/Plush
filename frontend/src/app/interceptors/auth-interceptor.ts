248c9503a583a3a2bd3ecc27a04270f3f12d4177
Caroline Gritsch <e11831765@student.tuwien.ac.at>
2024-05-21 12:11:30 +0000

    #66 Fixed Auth Interceptor to only intercept if a token is present

--- frontend/src/app/interceptors/auth-interceptor.ts
+++ frontend/src/app/interceptors/auth-interceptor.ts
@@ -15,15 +15,20 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const authUri = this.globals.backendUri + '/authentication';
 
-    // Do not intercept authentication requests
     if (req.url === authUri || req.url === authUri + '/nonce') {
       return next.handle(req);
     }
 
-    const authReq = req.clone({
-      headers: req.headers.set('Authorization', 'Bearer ' + this.authService.getToken())
-    });
+    const token = this.authService.getToken();
 
-    return next.handle(authReq);
+    if (token) {
+      const authReq = req.clone({
+        headers: req.headers.set('Authorization', 'Bearer ' + token)
+      });
+
+      return next.handle(authReq);
+    } else {
+      return next.handle(req);
+    }
   }
 }
