760d4dede0d07f29912b5154bd9dac64bf6e89ea
Tobias Haas <e12220872@student.tuwien.ac.at>
2024-05-22 14:14:17 +0000

    #67 Implemented fake auth for e2e tests

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/datagenerator/TestAdminDataGenerator.java
@@ -0,0 +1,47 @@
+package at.ac.tuwien.sepr.groupphase.backend.datagenerator;
+
+import java.lang.invoke.MethodHandles;
+import java.util.Map;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import at.ac.tuwien.sepr.groupphase.backend.entity.User;
+import at.ac.tuwien.sepr.groupphase.backend.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+
+/**
+ * Generates the admin users if no user with the public key exists.
+ */
+@Component
+@Profile("generateData")
+public class TestAdminDataGenerator {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    // map of admins with name and public key
+    private static final Map<String, String> ADMINS = Map.of("test-admin",
+            "EFWHJmGb4Ybgo7cueShmv42mDKZTkCrFNTeTPikhxitM");
+    private final UserRepository userRepository;
+
+    public TestAdminDataGenerator(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostConstruct
+    private void generate() {
+        for (String name : ADMINS.keySet()) {
+            String publicKey = ADMINS.get(name);
+            userRepository.findUserByPublicKey(publicKey)
+                    .or(() -> {
+                        User admin = new User(true);
+                        admin.setPublicKey(publicKey);
+                        LOGGER.info("creating admin {} with public key {}", name, publicKey);
+                        return Optional.of(userRepository.save(admin));
+                    });
+        }
+    }
+
+}
--- e2e/cypress/e2e/adminPlushtoy.cy.js
+++ e2e/cypress/e2e/adminPlushtoy.cy.js
@@ -1,13 +1,13 @@
 context('plushtoy', () => {
-    //
-    // it('should get delete on button press', () => {
-    //     cy.loginAdmin();
-    //     cy.url().should('contain', '/message')
-    //     cy.goToAdminOverview();
-    //     cy.getBySel("deleteButton").its('length').then((num) => {
-    //         cy.getBySel("deleteButton").first().click()
-    //         cy.getBySel("deleteButton").should("have.length", num - 1)
-    //     })
-    // })
+
+    it('should get delete on button press', () => {
+        cy.loginAdmin();
+        cy.url().should('contain', '/')
+        cy.goToAdminOverview();
+        cy.getBySel("deleteButton").its('length').then((num) => {
+            cy.getBySel("deleteButton").first().click()
+            cy.getBySel("deleteButton").should("have.length", num - 1)
+        })
+    })
 
 });
