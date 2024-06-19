4df58a07312b036ad9d3ef6ebd5dd9d978293423
Lucas Walter <e12119061@student.tuwien.ac.at>
2024-06-19 17:28:33 +0000

    #91: Implement custom image support

--- frontend/src/app/components/admin/plushtoy/overview/overview.component.ts
+++ frontend/src/app/components/admin/plushtoy/overview/overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { ConfirmationDialogComponent } from 'src/app/components/util/confirmation-dialog/confirmation-dialog.component';
 import { PlushToyListDto } from 'src/app/dtos/plushtoy';
