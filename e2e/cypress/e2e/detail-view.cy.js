6b497bc505c21f9418019b4e5d1c3ae35669caaf
artificialsi <antonkrytoi228@gmail.com>
2024-06-21 10:42:46 +0200

    fixed test

--- e2e/cypress/e2e/detail-view.cy.js
+++ e2e/cypress/e2e/detail-view.cy.js
@@ -15,8 +15,9 @@ context('detail-view', () => {
         cy.visit('/#/detail/1');
         cy.getBySel('plushToyName').should('contain', 'TEST TIGER 0 Nr.1');
         cy.getBySel("plushToyDesc").should('contain', 'Cute lil gent');
-        cy.getBySel("plushToySize").should('contain', '0kg');
-        cy.getBySel("plushToyStrength").should('contain', '0');
+        cy.getBySel("plushToyWeight").should('contain', '2kg');
+        cy.getBySel("plushToySize").should('contain', 'MEDIUM');
+        cy.getBySel("plushToyStrength").find('.star').should('have.length', 2);
         cy.getBySel("plushToyHealth").should('contain', '100HP');
         cy.getBySel("plushToyPrice").should('contain', '0.01 SOL');
 
