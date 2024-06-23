22aa0c8d466ce493c11049446373741bd2cb50cb
Jakob Herbst <e11924458@student.tuwien.ac.at>
2024-06-23 12:46:27 +0200

    wait for login

--- e2e/cypress/e2e/cart.cy.js
+++ e2e/cypress/e2e/cart.cy.js
@@ -7,34 +7,37 @@ context('cart', () => {
     });
 
     it('should add item to cart', () => {
-        cy.loginUser();
-        cy.goToDetailView();
+        cy.loginUser().then(() => {
+            cy.goToDetailView();
 
-        cy.getBySel("addToCartButton").click();
-        cy.get('.ng-trigger').should('contain', 'Item added to cart')
+            cy.getBySel("addToCartButton").click();
+            cy.get('.ng-trigger').should('contain', 'Item added to cart');
+        });
     });
 
     it('should verify item is in cart and check stats', () => {
-        cy.loginUser();
-        cy.goToCart();
+        cy.loginUser().then(() => {
+            cy.goToCart();
 
-        cy.get('.cart-item').should('exist');
-        cy.get('.item-name'). should( 'contain', '#1 TEST TIGER 0');
-        cy.get('.hp').should( 'contain', '100HP');
-        cy.get('.item-price').should( 'contain', '0.01 SOL');
-        cy.get('.total-price').should( 'contain', '0.01 SOL');
-        cy.get('.finish-payment').should('be.visible').and('be.enabled');
+            cy.get('.cart-item').should('exist');
+            cy.get('.item-name'). should( 'contain', '#1 TEST TIGER 0');
+            cy.get('.hp').should( 'contain', '100HP');
+            cy.get('.item-price').should( 'contain', '0.01 SOL');
+            cy.get('.total-price').should( 'contain', '0.01 SOL');
+            cy.get('.finish-payment').should('be.visible').and('be.enabled');
+        });
     }) ;
 
     it('should remove item from cart and verify cart is empty', () => {
-        cy.loginUser();
-        cy.goToCart();
+        cy.loginUser().then(() => {
+            cy.goToCart();
 
-        cy.get('.cart-item').its('length').then ((num) => {
-            cy.get('.remove-btn').last().click();
-            cy.get('.cart-item') .should ('have.length', num - 1);
+            cy.get('.cart-item').its('length').then ((num) => {
+                cy.get('.remove-btn').last().click();
+                cy.get('.cart-item') .should ('have.length', num - 1);
+            });
+            cy.get('.cart-item').should('not.exist');
+            cy.get('.total-price').should( 'contain', '0 SOL');
         });
-        cy.get('.cart-item').should('not.exist');
-        cy.get('.total-price').should( 'contain', '0 SOL');
     });
 });
